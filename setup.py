abd18e687683416c09e91746e7e593ee4db1edda
Prashant <prashantverma813@gmail.com>
2025-04-19 23:42:37 +0530

    added egg file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mlproject",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'flask==2.0.1',
+        'scikit-learn==1.0.2',
+        'pandas==1.3.4',
+        'numpy==1.21.2',
+        'datasets==2.0.0',
+        'tqdm==4.62.3',
+        'joblib==1.1.0',
+        'python-dotenv==0.19.0'
+    ],
+)
